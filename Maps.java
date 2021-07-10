8985cba128fa1ac53ba7ff4b20ef1240576e2cab
SrinivasRaman2001 <87238775+SrinivasRaman2001@users.noreply.github.com>
2021-07-10 22:21:06 +0530

    Add files via upload

---
+++ Maps.java
@@ -0,0 +1,17 @@
+//import org.apache.poi.xssf.usermodel.XSSFSheet;
+//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class Maps {
+//    public static void main(String[] args) {
+//
+//        XSSFWorkbook workbook= new XSSFWorkbook();
+//        XSSFSheet sheet=workbook.createSheet("Students data");
+//
+//        Map<String,String> data = new HashMap();
+//        data.put();
+//
+//    }
+//}
